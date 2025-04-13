f3b6b45b2a2a0485673b1953137ba120f656ca71
Mauricio S Otta <mauricio@portnumber53.com>
2025-04-13 02:16:18 -0700

    PR feedback

--- website/lib/xata.ts
+++ website/lib/xata.ts
@@ -1,9 +1,16 @@
 import { XataClient } from '../vendor/xata';
 
-const xata = new XataClient({
+const assertEnvVar = (name: string, value?: string): string => {
+  if (!value) {
+    throw new Error(`${name} environment variable is not defined`);
+  }
+  return value;
+};
+
+const xata = new XataClient({ 
   branch: process.env.XATA_BRANCH || 'main',
-  apiKey: process.env.XATA_API_KEY,
-  databaseURL: process.env.XATA_DATABASE_URL
+  apiKey: assertEnvVar('XATA_API_KEY', process.env.XATA_API_KEY),
+  databaseURL: assertEnvVar('XATA_DATABASE_URL', process.env.XATA_DATABASE_URL)
 });
 
 export default xata;
